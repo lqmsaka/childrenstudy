e6d5a7bc12fc53fcfd8c9b359f426f05caee53d8
Saka <sakas@sohu.com>
2020-10-22 16:48:52 +0800

    init

---
+++ data.js
@@ -0,0 +1,15 @@
+let content={
+	math:null,
+	chinese:null,
+	english:{
+		oxford:{
+			third:{
+				miu1:[
+					{en:"Hello",cn:"你好",s:"helou"},
+					{en:"you",cn:"你",s:"u"}
+				]
+			}
+		},
+		newconcept:null
+	}
+}
