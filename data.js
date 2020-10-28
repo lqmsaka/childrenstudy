fda0594b8d6a07ab96ca7994a9682a1668d82dce
Saka <sakas@sohu.com>
2020-10-28 21:02:46 +0800

    添加数据,调整显示样式,支持多个答案

--- data.js
+++ data.js
@@ -1,17 +1,64 @@
 let content={
-	math:null,
-	chinese:null,
-	english:{
-		oxford:{
-			second:{},
+	Math:null,
+	Chinese:null,
+	English:{
+		Example:[
+			{en:"Hello",cn:"你好",s:"helou"},
+			{en:"you",cn:"你",s:"u"},
+			{en:"",cn:"",s:""},
+		],
+		Oxford:{
+			"2B":{},
 			"3A":{
-				m1u1:[
-					{en:"Hello",cn:"你好",s:"helou"},
-					{en:"you",cn:"你",s:"u"}
+				M2U2:[
+					{en:"my grandfather",cn:"我的爷爷",s:""},
+					{en:"an old grandmother",cn:"一个老奶奶",s:""},
+					{en:"a tall father",cn:"一个高的爸爸",s:""},
+					{en:"her mother",cn:"她的妈妈",s:""},
+					{en:"a little brother",cn:"一个小弟弟",s:""},
+					{en:"a big sister",cn:"一个大的姐姐",s:""},
+					{en:"your family",cn:"你的家庭",s:""},
+					{en:"his good friend",cn:"他的好朋友",s:""},
+					{en:"a picture of my family",cn:"一副我的家庭照片",s:""},
+					{en:["like eating cakes","like to eat cakes"],cn:"喜欢吃蛋糕",s:""},
+					{en:"play football very well",cn:"足球踢得很好",s:""},
+					{en:"Mike's kite",cn:"Mike的风筝",s:""},
+					{en:"fly high in the sky",cn:"在天上飞得很高",s:""},
+					{en:"seven new bicycles",cn:"七辆新的自行车",s:""},
+					{en:"Who is she? She's my grandmother.",cn:"他是谁？他是我的奶奶。",s:""},
+					{en:"Is your mother tall? Yes, she is. No, she isn't.",cn:"你的妈妈高吗？是，她是的。不，她不是。",s:""},
+					{en:"Is his brother fat? Yes, he is. No, he isn't.",cn:"你的哥哥胖吗？是，他是的。不，他不是。",s:""},
+					{en:"Nice to meet you.",cn:"很高兴遇见你。",s:""},
+					{en:"Nice to meet you, too.",cn:"我也很高兴遇见你。",s:""},
 				],
-				m1u2:[]
+				M2U3:[
+					{en:"black hair",cn:"黑色的头发",s:""},
+					{en:"brown eyes",cn:"棕色的眼睛",s:""},
+					{en:"little animals",cn:"小动物",s:""},
+					{en:"a big mouth",cn:"一张大嘴巴",s:""},
+					{en:"your ears",cn:"你的耳朵",s:""},
+					{en:"a short tail",cn:"一条短的尾巴",s:""},
+					{en:"a small nose",cn:"一个小鼻子",s:""},
+					{en:"tall and fat",cn:"又高又胖",s:""},
+					{en:"three mice",cn:"三只老鼠",s:""},
+					{en:"his big pig",cn:"他的大的猪",s:""},
+					{en:"play with me",cn:"和我玩",s:""},
+					{en:"about me",cn:"关于我",s:""},
+					{en:"play with my pink ball",cn:"玩我的粉色的球",s:""},
+					{en:"My eyes are red",cn:"我的眼睛是红的",s:""},
+					{en:"My tail is short",cn:"我的尾巴是短的",s:""},
+					{en:"My mouth is big, too",cn:"我的嘴巴是也是大的",s:""}
+				],
 			}
 		},
-		newconcept:null
+		NewConcept:{
+			Book1:{
+				Lesson21:[
+					{en:"give",cn:"给",s:"ɡɪv"},
+					{en:"which",cn:"哪一个",s:"wɪtʃ"},
+					{en:"one",cn:"一个",s:"wʌn"},
+				]
+			}
+		}
 	}
 }
